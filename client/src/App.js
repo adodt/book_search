2a238713e3251bff190825c36dbfdaee2d5955d7
Gator <allidodt@gmail.com>
2023-01-31 15:42:32 -0800

    found bug! missing package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
 import {
   ApolloClient,
   InMemoryCache,
@@ -33,25 +33,19 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<SearchBooks />} />
+            <Route path="/saved" element={<SavedBooks />} />
+            <Route
+              path="*"
+              element={<h1 className="display-2">Wrong page!</h1>}
+            />
+          </Routes>
+        </>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,76 +1,76 @@
 import { gql } from "@apollo/client";
 
-export const LOGIN_USER= gql`
-mutation login($email: String, $password: String) {
+export const LOGIN_USER = gql`
+  mutation login($email: String, $password: String) {
     login(email: $email, password: $password) {
-        token
-        user {
-            _id
-            username
-        }
+      token
+      user {
+        _id
+        username
+      }
     }
-}
+  }
 `;
 
 export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser: (username: $username, email: password: $password) {
-        token
-        user {
-            _id
-            username
-        }
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
-}
+  }
 `;
 
 export const SAVE_BOOK = gql`
-mutation saveBook(
+  mutation saveBook(
     $bookId: String
     $authors: [String]
     $description: String
     $title: String
     $image: String
     $link: String
-) {
+  ) {
     saveBook(
-        bookId: $bookId
-        authors: $authors
-        desccription: $description
-        title: $title
-        image: $image
-        link: $link
+      bookId: $bookId
+      authors: $authors
+      description: $description
+      title: $title
+      image: $image
+      link: $link
     ) {
-        _id
-        username
-        email
-        bookCount
-        savedBooks {
-            bookId
-            authors
-            description
-            title
-            image
-            link
-        }
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
-}
+  }
 `;
 
 export const REMOVE_BOOK = gql`
-mutation removeBook($bookId: String!) {
+  mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
-        _id
-        username
-        bookCount
-        savedBooks {
-            bookId
-            authors
-            description
-            title
-            image
-            link
-        }
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
-}
+  }
 `;
