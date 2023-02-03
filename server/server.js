f13e4a778b0fa927e47a2c167644ffc7495e9620
Gator <allidodt@gmail.com>
2023-02-03 15:04:59 -0800

    testing monodb

--- server/server.js
+++ server/server.js
@@ -34,4 +34,12 @@ const startApolloServer = async (typeDefs, resolvers ) => {
   });
 };
 
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost:27017/your-database-name',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+// );
+
 startApolloServer(typeDefs, resolvers);
